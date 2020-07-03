7528ff0e872a228ce7346462461e0021048c842d
Mahbuba Alkhazra Nimme <mahbuba102@gmail.com>
2020-07-02 20:21:22 -0400

    initial commit

---
+++ GITProject2/src/main/java/com/talentech/GITProject2/App.java
@@ -0,0 +1,15 @@
+package com.talentech.GITProject2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        
+    }
+}
